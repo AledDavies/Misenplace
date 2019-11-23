9a4c59ee2455496487c8cc83ca53650c10b6918d
Aled Davies <AledDavies@users.noreply.github.com>
2019-11-23 13:38:20 -0800

    Minor improvements

--- config/zsh/features/misenplace.zsh
+++ config/zsh/features/misenplace.zsh
@@ -1,3 +1,13 @@
 # Aliases to allow quick navigate around the Misenplace directory structure
 alias miz='cd ${MIZ_HOME}'
-alias miz-scratch='cd ${DOCUMENTS_HOME}/Scratch'
+alias miz-scratch='cd ${WORKSPACE_HOME}/Scratch'
+
+# Create a Empty Directory in the Scratch Folder
+scratch() {
+    if [[ -n "${1}" ]]; then
+        mkdir -p "${WORKSPACE_HOME}/Scratch/${1}" 
+        cd "${WORKSPACE_HOME}/Scratch/${1}"
+    else
+        echo "Usage: ${0} <folder name>"
+    fi
+}
