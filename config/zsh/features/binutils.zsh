b423e7bb31dbfcddee3b71a355cda2259f2f332e
Aled Davies <AledDavies@users.noreply.github.com>
2020-04-24 17:28:16 -0700

    Make sure that homebrew only runs on MacOS

--- config/zsh/features/binutils.zsh
+++ config/zsh/features/binutils.zsh
@@ -1,6 +1,10 @@
 # Make sure that the updated command line tools end up at
 # the front of the path
-if [ -d "$(brew --prefix coreutils)/libexec/gnubin" ]; then
-  path=("$(brew --prefix coreutils)/libexec/gnubin" $path)
-  export MANPATH="$(brew --prefix coreutils)/libexec/gnuman:${MANPATH}"
-fi
+
+if [ $MIZ_OS_TYPE  = "Darwin" ]; then
+  if [ -d "$(brew --prefix coreutils)/libexec/gnubin" ]; then
+    path=("$(brew --prefix coreutils)/libexec/gnubin" $path)
+    export MANPATH="$(brew --prefix coreutils)/libexec/gnuman:${MANPATH}"
+  fi
+fi 
+
